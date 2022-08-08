eb4667f9eb785da9857f1e334ea0b5d21bc51a1f
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-08 14:16:34 +0700

    Finish

---
+++ My Training/Kelas-Terbuka/04-Mengenal Variabel/Main.py
@@ -0,0 +1,25 @@
+# Variabel adalah tempat menyimpan data
+
+# Menaruh / Assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+print('Nilai a = ', a)
+print('Nilai x = ', x)
+print('Nilai panjang = ', panjang)
+
+# Penamaan
+nilai_y = 15  # dengan menggunakan underscore
+juta10 = 10000000  # tidak boleh menaruh angka di depan
+nilaiZ = 17.5
+
+# Pemanggilan Kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+
+# Assignment indirect
+b = a
+print("Nilai b = ", b)
