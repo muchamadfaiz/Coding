cba9c5efe2c36fb810fd809e52aa1455dbe5df9a
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-09 22:47:54 +0700

    Progress 12 (memeriksa angka kurang dari 3)

---
+++ My Training/Kelas-Terbuka/12-Latihan Komparasi dan Logika/Main.py
@@ -0,0 +1,12 @@
+# episode latihan logika dan komparasi
+
+# membuat gabungan area rentang dari angka
+
+# ++++++3------10++++++
+
+inputUser = float(input("masukan angka yang bernilai\n kurang dari 3 \natau \nlebih besar dari 10\n:"))
+
+# ++++++3------------
+# memeriksa angka kurang dari 3
+isKurangDari = inputUser < 3
+print(isKurangDari)
