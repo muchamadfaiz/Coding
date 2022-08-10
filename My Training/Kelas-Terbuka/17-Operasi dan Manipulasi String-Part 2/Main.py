f4fe6e555fdbce726cd78c30fda7e015e92e6a8c
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-10 18:26:00 +0700

    Progress 17

---
+++ My Training/Kelas-Terbuka/17-Operasi dan Manipulasi String-Part 2/Main.py
@@ -0,0 +1,23 @@
+# operator dalam bentuk methods
+
+## merubah case dari string
+
+# merubah semua ke uppercase
+
+salam = "bro"
+print("normal =", salam)
+salam = salam.upper()
+print("upper =", salam)
+
+# merubah semua ke lowercase
+# bisa pake .lower
+alay = "aKu keCe AbiezZZZ"
+print("normal =", alay)
+alay = alay.lower()
+print("lower =", alay)
+
+# bisa pake .casefold()
+alay = "aKu keCe AbiezZZZ"
+print("normal =", alay)
+alay = alay.casefold()
+print("lower =", alay)
