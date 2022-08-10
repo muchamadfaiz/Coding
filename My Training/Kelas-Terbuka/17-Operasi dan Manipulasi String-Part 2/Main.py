5497a0e4a73c7fcb0cd9964bba70573aaad6d9db
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-10 22:11:22 +0700

    Progress 17

--- My Training/Kelas-Terbuka/17-Operasi dan Manipulasi String-Part 2/Main.py
+++ My Training/Kelas-Terbuka/17-Operasi dan Manipulasi String-Part 2/Main.py
@@ -21,3 +21,40 @@ alay = "aKu keCe AbiezZZZ"
 print("normal =", alay)
 alay = alay.casefold()
 print("lower =", alay)
+
+## pengecekan dengan menggunakan isX method
+
+# contoh pengecekan lower case
+salam = "sist"
+apakah_lower = salam.islower() #hasilnya bool
+print(salam, "is lower =", apakah_lower)
+apakah_upper = salam.isupper() #hasilnya bool
+print(salam, "is upper =", apakah_upper)
+
+# isalpha() <-- untuk mengecek semuanya huruf
+# isalnum() <-- huruf dan angka
+# isdecimal() <-- angka saja
+# isspace() <-- spasi, tab, newline \n
+# istitle() <-- semua kata dimulai dengan huruf besar
+
+judul = "It Is Okay Not To Be Orkay" #huruf awal harus besar semua utnuk dapet nilai True
+cek_judul = judul.istitle()
+print(judul, "is title =", cek_judul)
+
+## ngecek komponen startswith() endswith() <-- keren
+cek_start = "Sangjangnim Oppa".startswith("Sangjangnim") #uppercase dll hrus sama untuk dapat nilai True
+print("start = ", cek_start)
+
+cek_end = "Sangjangnim Oppak".endswith("Oppak")
+print("end = ", cek_end)
+
+## penggabungan komponen join() split()
+pisah = ['aku','sayang','kamu']
+gabungan = " ".join(pisah)
+print(pisah)
+print(gabungan)
+
+gabungan = " ehm ".join(pisah)
+print(gabungan)
+
+
