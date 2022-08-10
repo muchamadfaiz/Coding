d27e5794a27493fdb08121219ef8cd5f77990665
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-10 17:55:16 +0700

    Finish 16

--- My Training/Kelas-Terbuka/16-Operasi dan Manipulasi String/Main.py
+++ My Training/Kelas-Terbuka/16-Operasi dan Manipulasi String/Main.py
@@ -1,6 +1,9 @@
 # Operasi dan manipulasi string
 
 #1. menyambung string
+from tarfile import LENGTH_LINK
+
+
 nama_pertama = "ucup"
 nama_tengah = "D"
 nama_akhir = "Fame"
@@ -31,13 +34,33 @@ print(d + " tidak ada di " + nama_lengkap + " = " + str(status))
 print("wk"*10)
 print(10*"wk")
 
-# indexing
+# indexing 
 print("index ke-0 : " +nama_lengkap[0])
-print("index ke-0 : " +nama_lengkap[1])
-print("index ke-0 : " +nama_lengkap[4])
-print("index ke-0 : " +nama_lengkap[5])
-print("index ke-0 : " +nama_lengkap[10])
-print("index ke-0 : " +nama_lengkap[-1])
-print("index ke-0 : " +nama_lengkap[-2])
+print("index ke-1 : " +nama_lengkap[1])
+print("index ke-4 : " +nama_lengkap[4])
+print("index ke-5 : " +nama_lengkap[5])
+print("index ke-10 : " +nama_lengkap[10])
+print("index ke -1: " +nama_lengkap[-1])
+print("index ke-2: " +nama_lengkap[-2])
+print("index ke-[0:3]: " +nama_lengkap[0:3]) # : artinya sampai
+print("index ke-[3:8]: " +nama_lengkap[3:8]) # : artinya sampai
+print("index ke-[0,2,4,6,8,10]: " +nama_lengkap[0:10:2]) # : artinya 0 sampai 10 dengan increment 2
 
+# item paling kecil
+print("paling kecil : " + min(nama_lengkap))
+# item paling besar
+print("paling bear : " + max(nama_lengkap))
+
+ascii_code = ord(" ")
+print ("ASCII code untuk spasi adalah ",ascii_code)
+ascii_code = ord("'")
+print ("ASCII code untuk spasi adalah ",ascii_code)
+data = 117
+print("char untuk ASCII 117 adalah", chr(data))
+
+# 4. Operator dalam bentuk method
+
+data = "otong surotong pararotong"
+jumlah = data.count("o")
+print("jumlah o pada data =", jumlah)
 
