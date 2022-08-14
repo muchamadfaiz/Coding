363e1bae06c19316b9df64fdaf71f60dcbb01593
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-14 23:43:16 +0700

    Finish 22

---
+++ My Training/Kelas-Terbuka/22-ELIF Statement/Main.py
@@ -0,0 +1,13 @@
+# ELIF = ELSE IF Statement
+
+nama = input("Nama kamu siapa? ")
+
+if nama == "pawpaw": # kondisi 1
+    print(f"{nama} kamu ganteng banget") #aksi true 1
+elif nama == "faiz": # kondisi 2
+    print(f"{nama} kamu juga ganteng banget") #aksi true 2
+elif nama == "umiw": # kondisi 3
+    print(f"{nama} mah cantik") #aksi true 2
+else :
+    print(f"siapa si {nama} ga kenal")
+print("akhir dari program")
