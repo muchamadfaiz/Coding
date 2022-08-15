30e428f8661c0c13f6a3069ff542095bf3dacac1
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-15 14:09:46 +0700

    Finish 04

--- My Training/CodewithMosh/2. Primitive Types/04-Escape Sequences/app.py
+++ My Training/CodewithMosh/2. Primitive Types/04-Escape Sequences/app.py
@@ -1,15 +1,16 @@
-#escape character (\")
+#escape character
+# (\")
 course = "Python \"Programming" 
 print(course)
 
-#escape character (\')
+# (\')
 course = "Python \'Programming" 
 print(course)
 
-#escape character (\\)
+# (\\)
 course = "Python \\Programming" 
 print(course)
 
-#escape character (\n)
+# (\n)
 course = "Python \nProgramming" 
 print(course)
