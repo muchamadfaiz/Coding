a786741b37365d3eaaa29166f4ccf6b0715913d0
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-15 14:19:18 +0700

    Finish 05

---
+++ My Training/CodewithMosh/2. Primitive Types/05-Formatted Strings/app.py
@@ -0,0 +1,17 @@
+#Concatenation
+first = "Muchamad"
+last = "Faiz"
+full = first + " " + last #Concatenation
+print(full)
+
+#Formatted String
+first = "Muchamad"
+last = "Faiz"
+full = f"{first} {last}"
+print(full)
+
+#we can put any value expression in between curly braces
+first = "Muchamad"
+last = "Faiz"
+full = f"{len(first)} {2+2}"
+print(full)
