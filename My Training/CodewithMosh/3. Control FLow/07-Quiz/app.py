9c1db59f792d666e133a71c6b155c91a7a031531
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-16 00:32:20 +0700

    Finish 07

---
+++ My Training/CodewithMosh/3. Control FLow/07-Quiz/app.py
@@ -0,0 +1,13 @@
+if 10 == "10":
+    print("a")
+elif "bag" > "apple" and "bag" > "cat":
+    print("b")
+else:
+    print("c")
+
+# a = ord("b")
+# b = ord("C")
+# c = ord("a")
+# print(a)
+# print(b)
+# print(c)
