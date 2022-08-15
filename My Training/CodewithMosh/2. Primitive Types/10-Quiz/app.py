a47cdc813a5caebe31ccbbff91c4d5275be44ff5
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-15 22:29:16 +0700

    Finish 10

---
+++ My Training/CodewithMosh/2. Primitive Types/10-Quiz/app.py
@@ -0,0 +1,11 @@
+# QUIZ
+# 1. >> What are the primitive types in Python?
+# 1. String
+# 2. Numbers (Integer, Float, complex number)
+# 3. Boolean
+
+fruit = "Apple"
+print(fruit[1])
+print(fruit[1:-1])
+10 % 3
+print(bool("False"))
