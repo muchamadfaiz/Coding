80d043147202b40302a3f6a21d70aa4fc0b7fbd2
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-16 00:07:50 +0700

    Finish 03

---
+++ My Training/CodewithMosh/3. Control FLow/03-Ternary Operators/app.py
@@ -0,0 +1,22 @@
+# Ternary Operator 
+
+age = 10
+# if age >= 18:
+#     print("Eligible")
+# else:
+#     print("Not eligible")
+
+# Cleaner Code
+# 1.
+# age = 22
+# if age >= 18:
+#     message = "Eligible"
+# else:
+#     message = "Not eligible"
+# print(message)
+
+# 2.
+message = "Eligible" if age >= 18 else "Not Eligible" # This is ternary operator
+print(message)
+
+    
