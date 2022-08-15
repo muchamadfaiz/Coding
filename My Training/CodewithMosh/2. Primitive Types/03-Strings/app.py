a416ddb0f75c0428fa0b5bf3f650142fcb390f8d
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-15 13:45:12 +0700

    Finish 03

---
+++ My Training/CodewithMosh/2. Primitive Types/03-Strings/app.py
@@ -0,0 +1,24 @@
+# Triple quotes (long message)
+# message = """
+# Hi Mosh,
+
+# This is Faiz from flitzcode
+
+# blah blah
+# """
+
+# Built in Function in Python
+# Function = a reusable piece of code that carries a task
+# always ends up with paranthesis indicates calling function
+# on parathesis we can fill argument (inputs to the function)
+# Print (), ln(), input(), etc
+
+course = "Python Programming"
+print(len(course))
+print(course[0]) #we use square bracket to get acces to specific character in a string, we get first character
+print(course[-1]) #we can use negative string,
+print(course[0:3]) #to slice a string, start:end
+print(course[0:]) #start:end
+print(course[:3]) #start:end, default is zero
+print(course[:]) #start:end, default is zero
+
