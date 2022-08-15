cd441ccb4812619efb92f11ea325dbd0ea04b030
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-16 00:20:28 +0700

    Finish 05

---
+++ My Training/CodewithMosh/3. Control FLow/05-Short Circuit Evaluation/app.py
@@ -0,0 +1,10 @@
+high_income = True
+good_credit = True
+student = False
+
+if high_income or good_credit and not student:
+    print("eligible")
+else:
+    print("not eligible")
+
+# in Pythn logical operator is short circuit
