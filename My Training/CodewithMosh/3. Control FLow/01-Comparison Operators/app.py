0e62432f4fe94eaa25665313666ed5b4ba2d4938
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-15 23:02:27 +0700

    Finish 01

---
+++ My Training/CodewithMosh/3. Control FLow/01-Comparison Operators/app.py
@@ -0,0 +1,27 @@
+# Comparison Operators
+a1 = 10 > 3
+a2 = 10 >= 3
+a3 = 10 < 20
+a4 = 10 <= 3
+a5 = 10 == 10
+a6 = 10 == "10"
+a7 = 10 != "10"
+a8 = "bag" > "apple"
+a9 = "bag" == "BAG"
+
+# Numeric Representation
+a10 = ord("b")
+a11 = ord("B")
+
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+print(a5)
+print(a6)
+print(a7)
+print(a8)
+print(a9)
+print(a10)
+print(a11)
+
