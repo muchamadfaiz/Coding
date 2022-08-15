eb90a7fde0d899389144fb408db374c5575dca8b
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-16 00:52:33 +0700

    Finish 09

---
+++ My Training/CodewithMosh/3. Control FLow/09-For Else/app.py
@@ -0,0 +1,14 @@
+# For Else
+successful = True
+for number in range(1, 4):
+    print("Attempt", number)
+    if successful:
+        print("Successful")
+        break
+else:
+   print("Attempted 3 times and failed") 
+    
+
+
+
+
