5f9dd534ca6d055bcc10f1c3a93111c460974579
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-16 00:44:06 +0700

    Finish 08

---
+++ My Training/CodewithMosh/3. Control FLow/08-For Loops/app.py
@@ -0,0 +1,15 @@
+# For Loops
+# Use loops to create repetition
+for number in range(3):
+    print("Attempt", number + 1, (number + 1) * "." )
+
+# make it cleaner
+for number in range(1, 4):
+    print("Attempt", number, number * "." )
+
+# with steps
+for number in range(1, 10, 2):
+    print("Attempt", number, number * "." )
+
+
+
