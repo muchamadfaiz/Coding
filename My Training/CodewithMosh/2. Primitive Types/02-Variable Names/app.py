f7fdfd3d197a550c87e091a1723bb62c61e971cb
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-15 12:35:19 +0700

    Finish 02

---
+++ My Training/CodewithMosh/2. Primitive Types/02-Variable Names/app.py
@@ -0,0 +1,9 @@
+# variable names always descriptive
+# variable names always lowercase (PEP 8)
+# variable names always use underscore for space (PEP 8)
+
+students_count = 1000 # integer
+rating = 4.99 # fLoat
+is_published = False # boolean
+course_name = "Python Programming" 
+print(students_count)
