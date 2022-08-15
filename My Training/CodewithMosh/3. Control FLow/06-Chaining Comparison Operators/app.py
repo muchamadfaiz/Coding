5452fce2b7dbf98dd6fde9fe86e52784c1f26eee
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-16 00:24:44 +0700

    Finish 06

---
+++ My Training/CodewithMosh/3. Control FLow/06-Chaining Comparison Operators/app.py
@@ -0,0 +1,9 @@
+# chaining comparison operator
+# its very powerful technique to make a clean code
+
+# age should be between 18 and 65
+age = 22
+# if age <= 18 and age < 65: #nornal
+if 18 <= age < 65: # chaining comparison operator
+    print("eligible")
+
