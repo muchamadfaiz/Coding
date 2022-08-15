734e2f3877dbefdbf2025db1c6bbf65e15bf0f0c
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-15 23:07:41 +0700

    Finish 02

---
+++ My Training/CodewithMosh/3. Control FLow/02-Conditional Statements/app.py
@@ -0,0 +1,10 @@
+temperature = 25
+if temperature > 30:
+    print("it's warm")
+    print("Drink Water")
+elif temperature > 20:
+    print("It's Nice")
+else:
+    print("It's cold")
+
+print("Done")
