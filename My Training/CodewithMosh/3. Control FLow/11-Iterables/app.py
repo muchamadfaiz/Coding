91abb076f16ad2a862f6cc11556bd371db1daf34
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-16 19:57:35 +0700

    Finish 11

---
+++ My Training/CodewithMosh/3. Control FLow/11-Iterables/app.py
@@ -0,0 +1,20 @@
+#Complex Type
+"""
+1. Range
+2. List
+3. 
+"""
+
+# print(type(5)) # Class Integer
+# print(type(range(5))) # Class Range
+
+# Iterable
+
+# for x in range (5): # Iterable in Range
+#     print(x)
+
+# for x in "Python": # Iterable in String
+#     print(x)
+
+for x in [1, 2, 3, 4, 5]: # Iterable in List
+    print(x)
