d85e0810e7141a033bd4447a501cf59bfbce79cd
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-17 09:21:34 +0700

    Finish 01

---
+++ My Training/CodewithMosh/4. Functions/01-Defining Functions/app.py
@@ -0,0 +1,7 @@
+# How to write your own function
+def greet():  # your function must meaningful, descriptive, separate with underscore and use lower case
+    print("Hi there")
+    print("welcome abroad")
+
+    
+greet()
