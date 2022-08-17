cfebee8ddb86646b5350ee6c6639a7e901c3c61c
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-17 10:09:55 +0700

    Finish 02

---
+++ My Training/CodewithMosh/4. Functions/02-Arguments/app.py
@@ -0,0 +1,8 @@
+# How to write your own function
+def greet(first_name, last_name):  # your function must meaningful, descriptive, separate with underscore and use lower case
+    print(f"Hi {first_name} {last_name}")
+    print("welcome abroad")
+
+    
+greet("Muchamad", "Faiz")
+greet("Herviana", "Nurulita")
