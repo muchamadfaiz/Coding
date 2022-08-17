cdf96c807ef083175b51ed74d5a5f8086fb1ac92
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-17 22:03:59 +0700

    Finish 13

---
+++ My Training/CodewithMosh/5. Data Structures/13-Stacks/app.py
@@ -0,0 +1,23 @@
+# Stacks
+# browsing_session = []
+# browsing_session.append(1)
+# browsing_session.append(2)
+# browsing_session.append(3)
+# print(browsing_session)
+# last = browsing_session.pop()
+# print(last)
+# print(browsing_session)
+# print("redirect", browsing_session[-1])
+# browsing_session.pop()
+# browsing_session.pop()
+# if not browsing_session:
+#     print("disable")
+
+# Recap
+browsing_session = []
+browsing_session.append(1) # to add an item on top of the stack
+browsing_session.pop() # to remove the item on top of the stack
+if not browsing_session: # checking whether there is an item on the stack
+    browsing_session[-1] # to get item on top of the stack
+print("disable")
+
