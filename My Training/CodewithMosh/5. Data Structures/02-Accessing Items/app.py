ac4b01a514e61c1866fd36a9786d8c6b5e41f999
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-17 19:03:51 +0700

    Finish 02

---
+++ My Training/CodewithMosh/5. Data Structures/02-Accessing Items/app.py
@@ -0,0 +1,15 @@
+# Accessing Items using square bracket
+# letters = ["a", "b", "c", "d"]
+# letters[0] = "A"
+# print(letters)
+# print(letters[0:3])
+# print(letters[:3])
+# print(letters[0:])
+# print(letters[:])
+# print(letters[::2])
+
+numbers = list(range(20))
+print(numbers)
+print(numbers[::2])
+print(numbers[::-1])
+print(numbers[::1])
