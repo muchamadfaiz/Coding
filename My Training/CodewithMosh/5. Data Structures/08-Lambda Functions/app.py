37a3a9a20f9e361bdda77dfecfffeb038b11644e
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-17 19:59:30 +0700

    Finish 08

---
+++ My Training/CodewithMosh/5. Data Structures/08-Lambda Functions/app.py
@@ -0,0 +1,22 @@
+# Lambda Function
+# lambda = anonymous function
+from ast import Expression
+
+
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12),
+]
+
+# def sort_item(items):
+#     return items[1]
+
+# items.sort(key = sort_item, reverse=True)
+# print(items)
+
+# cleaner code
+# items.sort(key=lambda parameters:Expression)
+items.sort(key=lambda item:item[1])
+print(items)
+
