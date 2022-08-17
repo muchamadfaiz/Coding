5b0ba48279d152e979385347aeb65e8a7d54c0c1
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-17 15:09:51 +0700

    Finish 10

---
+++ My Training/CodewithMosh/4. Functions/10-VSCode Coding Tricks-Window/app.py
@@ -0,0 +1,11 @@
+"""
+Shortcut for Windows
+1. Shift + Alt + up/downArrow --> copy line
+2. Alt + up/downArrow --> move line
+3. Home --> beginning of line
+4. End --> end of line
+5. Ctrl + Home --> beginning og file
+6. Ctrl + End --> end of file
+7. Ctrl + Enter --> add new line below
+8. Ctrl + Shift + Enter --> add new line above
+"""
