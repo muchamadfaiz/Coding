f10748183f26f8c64e8f71dbf3065a780a17c046
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-18 18:20:05 +0700

    Finish 27

---
+++ My Training/Kelas-Terbuka/27-Break/main.py
@@ -0,0 +1,48 @@
+# Break
+# langsung ke akhir program
+
+# Program 1
+# angka = 0
+
+# while angka < 5:
+#     angka += 1
+#     print(f"angka sekarang --> {angka}")
+#     print("pawpawganteng")
+#     if angka == 3:
+#         print("Faiz Ganteng")
+#         # break
+# print(angka)
+# print("akhir program cui")
+
+# Program 2
+
+# data_input = int(input("hitung sampai= "))
+# angka = 0
+
+# while True:
+#     angka += 1
+#     print(f"angka sekarang --> {angka}")
+
+#     if angka == data_input:
+#         print("Faiz Ganteng")
+#         break
+#     print("hai")
+
+# print(angka)
+
+# Program 3
+
+data_input = int(input("hitung sampai= "))
+angka = 0
+
+while angka < data_input:
+    angka += 1
+    print(f"angka sekarang --> {angka}")
+
+    if angka == data_input:
+        print("Faiz Ganteng")
+        # break
+    print("ada terus setiap loop")
+
+print(angka)
+print("akhir program cui")
