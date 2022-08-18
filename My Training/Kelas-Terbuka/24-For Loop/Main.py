49c9ea991fd6353e9000be2c816dc28533b62e1e
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-18 17:22:11 +0700

    Finish 24

--- My Training/Kelas-Terbuka/24-For Loop/Main.py
+++ My Training/Kelas-Terbuka/24-For Loop/Main.py
@@ -1,1 +1,34 @@
 # Perulangan (Loop)
+
+# for kondisi:
+#   aksi
+
+# dengan list
+# angka2_list = [0,1,2,3,4]
+
+# for i in angka2_list:
+#     print (f"i sekarang -> {i}")
+
+# print("akhir dari program 1\n")
+
+# # dengan range
+# angka2_range = range(5)
+# for i in angka2_range:
+#     print (f"i sekarang -> {i}")
+
+# print("akhir dari program 2\n")
+
+# # dengan range
+# angka2_range = range(1,5)
+# for i in angka2_range:
+#     print (f"i sekarang -> {i}")
+#     print ("faiz keren")
+
+# print("akhir dari program 3\n")
+
+# menggunakan string
+
+data_str = "faiz ganteng abizz"
+
+for huruf in data_str:
+    print(huruf)
