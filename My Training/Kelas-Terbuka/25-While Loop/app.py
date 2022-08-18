4b8d7f20a551f7a8b82118845d86636095eb4440
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-18 17:35:24 +0700

    Finish 25

---
+++ My Training/Kelas-Terbuka/25-While Loop/app.py
@@ -0,0 +1,20 @@
+# while loop
+
+from mimetypes import init
+from struct import iter_unpack
+
+
+# while kondisi:
+#     aksi ini
+#     aksi itu
+
+# akhir dari program
+
+angka = 0
+print(f"angka sekarang {angka}")
+
+while angka < 5:
+    angka += 1
+    print(f"Faiz Ganteng ini loop ke {angka}")
+
+print("cukup")
