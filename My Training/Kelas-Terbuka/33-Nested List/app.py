86b3f10461957282b0ebd6448ff85efdd82482d1
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-19 23:48:44 +0700

    Finish 33

---
+++ My Training/Kelas-Terbuka/33-Nested List/app.py
@@ -0,0 +1,33 @@
+data_0 = [1,2]
+data_1 = [3,4,5]
+
+data_list_biasa = [1,2,3,4]
+
+print(f"list biasa = {data_list_biasa}")
+
+list_2D = [data_0,data_1,6,7]
+
+print(f"list 2D = {list_2D}")
+
+#contoh penggunaan
+
+peserta_0 = ["Faiz", 28, "Laki-laki"]
+peserta_1 = ["Pawpaw", 20, "Laki-laki"]
+peserta_2 = ["Umiw", 29, "Perempuan"]
+
+list_peserta = [peserta_0, peserta_1, peserta_2]
+print(f"peserta = {list_peserta}")
+
+for peserta in list_peserta:
+    print(f"nama\t:{peserta[0]}")
+    print(f"umur\t:{peserta[1]}")
+    print(f"gender\t:{peserta[2]}\n")
+
+# dengan reference
+
+list_copy = list_peserta.copy()
+print(f"peserta = {list_copy}")
+peserta_0[0] = "bilbil"
+print(f"peserta = {list_copy}")
+print(f"peserta = {list_peserta}")
+
