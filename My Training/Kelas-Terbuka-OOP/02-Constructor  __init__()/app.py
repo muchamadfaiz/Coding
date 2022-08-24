b129a805793cef7b51ef4fa3e3db0e1dff6a62fe
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-24 21:06:04 +0700

    Finish 02

--- My Training/Kelas-Terbuka-OOP/02-Constructor  __init__()/app.py
+++ My Training/Kelas-Terbuka-OOP/02-Constructor  __init__()/app.py
@@ -1,9 +1,33 @@
 class Hero: #template / blueprint for creating object
     
-    def __init__(self,x):
-        print("hallo",x)
+    def __init__(self, inputName, inputPower, inputHealth, inputArmor): # akan di eksekusi saat si object di buat
+        # print("hallo",x) # self adalah si hero1
+        self.name = inputName
+        self.power = inputPower
+        self.health = inputHealth
+        self.armor = inputArmor
 
-hero2 = Hero(10)
+# hero1 = Hero(10) # hero1 --> object
+hero1 = Hero("sniper",100,10,4) # hero1 --> object ke-1
+hero2 = Hero("mirana",100,15,1) # hero1 --> object ke-2
+hero3 = Hero("faiz",1000,100,0) # hero1 --> object ke-3, jadi ada 3 objek
 
+print(hero1.__dict__) # untuk melihat atrribut yang ada di objek tsb
+print(hero1.name)
+print(hero1.power)
+print(hero1.health)
+print(hero1.armor)
+
+print(hero2.__dict__) # untuk melihat atrribut yang ada di objek tsb
+print(hero2.name)
+print(hero2.power)
+print(hero2.health)
+print(hero2.armor)
+
+print(hero3.__dict__) # untuk melihat atrribut yang ada di objek tsb
+print(hero3.name)
+print(hero3.power)
+print(hero3.health)
+print(hero3.armor)
 
 
---
+++ My Training/Kelas-Terbuka-OOP/referensi/app.py
@@ -0,0 +1,129 @@
+"""
+Tanpa OOP
+"""
+# programmer = "eka"
+# def programer_makan():
+#     print(f"{programmer} makan nasi")
+
+# petani = "putra"
+# def petani_makan():
+#     print(f"{petani} makan nasi")
+
+# dokter = "putri"
+# def dokter_makan():
+#     print(f"{dokter} makan nasi")
+
+# programer_makan()
+# petani_makan()
+# dokter_makan()
+
+"""
+Mulai OOP
+"""
+class Manusia:
+    nama = "None" # Properties
+
+    def __init__(self,nama):
+        self.nama = nama
+
+    def makan (self): # Fungsi/Method 
+        print(f"{self.nama} makan nasi")
+
+# programmer = Manusia("eka")
+# programmer.makan()
+
+# dokter = Manusia("debay")
+# dokter.makan()
+
+# influencer = Manusia("umiw")
+# influencer.makan()
+
+# """
+# INHERITANCE
+# """
+# class ManusiaMilenial(Manusia): # manusia milenial merupakan subclass dari Manusia(superclass)
+#     email = "None"
+
+#     def set_email(self, email): 
+#         self.email = email
+
+#     def info(self): 
+#         print(f"nama:{self.nama}, email = {self.email}")
+
+# programmer = ManusiaMilenial("eka")
+# programmer.set_email("ekatest.com")
+# programmer.makan()
+# programmer.info()
+
+# dokter = ManusiaMilenial("debay")
+# dokter.set_email("dokter.com")
+# dokter.makan()
+# dokter.info()
+
+# influencer = ManusiaMilenial("umiw")
+# influencer.set_email("umiw.com")
+# influencer.makan()
+# influencer.info()
+
+
+"""
+ENCAPSULATION
+"""
+# class ManusiaMilenial(Manusia): # manusia milenial merupakan subclass dari Manusia(superclass)
+#     email = "None"
+#     __pasword = "None"
+
+#     def set_email(self, email): 
+#         self.email = email
+
+#     def set_pass(self, password):
+#         self.__password = password
+
+#     def __samarkan_password(self):
+#         return self.__password.replace("a", "*")
+
+#     def info(self): 
+#         print(f"nama:{self.nama}, email = {self.email}\npassword = {self.__samarkan_password()}")
+
+# programmer = ManusiaMilenial("eka")
+# programmer.set_email("ekatest.com")
+# programmer.set_pass("rahasia")
+# programmer.info()
+
+"""
+POLYMORPHISM
+"""
+
+class ManusiaMilenial(Manusia): 
+    email = "None"
+    __pasword = "None"
+
+    def set_email(self, email): 
+        self.email = email
+
+    def set_pass(self, password):
+        self.__password = password
+
+    def __samarkan_password(self):
+        return self.__password.replace("a", "*")
+
+    def info(self): 
+        print(f"nama:{self.nama}, email = {self.email}\npassword = {self.__samarkan_password()}")
+
+class Programmer(ManusiaMilenial): 
+    def info(self): 
+        print(f"nama:{self.nama}/ email = {self.email}") # beda di penggunaan slash "/"
+
+class Influencer(ManusiaMilenial):
+    def info(self): 
+        print(f"nama:{self.nama}| email = {self.email}") # beda di penggunaan "|"
+
+programmer = Programmer("eka")
+programmer.set_email("ekatest.com")
+programmer.set_pass("rahasia")
+programmer.info()
+
+Influencer = Influencer("pawpaw")
+Influencer.set_email("pawpaw.com")
+Influencer.set_pass("aafadffaf")
+Influencer.info()
