5c243cd88576fa65617c3ea733c2b8bd30fc854a
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-24 11:26:56 +0700

    Dasar OOP

---
+++ My Training/Computer Science/OOP/app.py
@@ -0,0 +1,44 @@
+"""
+What is an object?
+in real world --> A thing from the real world
+in programming --> A thing that you want to store and process data
+in others --> Another name for an object is an entity
+
+Entitas dalam informatika adalah benda, orang, tempat, 
+unit, objek atau hal lainnya yang mempresentasikan data, 
+dan data tersebut akan disimpan ke dalam pangkal data. 
+Setiap entitas terdiri dari beberapa atribut contohnya 
+entitas karyawan maka memiliki atribut nama, karyawan, alamat, dan id.
+
+Fundamental concept OOP
+1. Abstraction -> menyederhanakan realita dan fokus hanya pada data dan proses yang berhubungan dengan aplikasi yang dibuat
+ex: orang adalah sebuah "objek", jika kamu ingin membuat sebuah aplikasi untuk meproses data
+tentang seseorang, tidak harus kamu tau semua tentang orang tersebut melainkan hanya hal-hal yang 
+berhubungan dan tugas yang dikerjakan untuk data tersebut
+
+untuk membuat object secara tata bahasa programming yang benar kamu butuh sebuah "class"
+Class -> adalah blueprint/cetakan untuk membuat suatu "objek"
+      -> adalah sebuah kode yg ditulis programmer untuk mendefinisikan attribut/sifat 
+         dan operasi dari objek tersebut
+
+Attribut/Properties -> mendeskripsikan sebuah "objek" terkadang direferensikan sebagai "field" karena 
+            mengandung data
+ex: FirstName:
+    LastName:
+    Gender:
+    DateOfBirth:
+    Occupation:
+
+Operations/Behaviours -> sebuah aksi yang di lakukan oleh "objek" tersebut    
+Method -> sebuah program di dalam class yang dibuat kode sebagai fungsi atau prosedur
+ex: PayWages()
+    BookHolidays()
+    Appraisal()
+    Promotion()
+    SaveDetails()
+
+2. Encapsulation -> kompleksitas program nya di sembunyikan, jadi yang pake tinggal tau interface data nya aja
+3. Inheritance -> sebuah "class" dapat memperoleh metode dan properties dari "class" yang lain 
+menghasilkan sebuah hierarki "class" (subclass and superclass)
+4. Polymorphism -> 
+"""
