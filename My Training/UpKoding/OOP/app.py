39ce8047ea373646039a0d60890afd77834f3700
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-24 10:35:11 +0700

    memulai OOP

---
+++ My Training/UpKoding/OOP/app.py
@@ -0,0 +1,15 @@
+programmer = "eka"
+def programer_makan():
+    print(f"{programmer} makan nasi")
+
+petani = "putra"
+def petani_makan():
+    print(f"{petani} makan nasi")
+
+dokter = "putri"
+def dokter_makan():
+    print(f"{dokter} makan nasi")
+
+programer_makan()
+petani_makan()
+dokter_makan()
