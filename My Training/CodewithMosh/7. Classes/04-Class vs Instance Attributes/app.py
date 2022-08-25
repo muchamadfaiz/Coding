cd08d8e4c25fba963f2f4e1aa5898559fc65262b
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-25 16:03:59 +0700

    Finish 04

--- My Training/CodewithMosh/7. Classes/04-Class vs Instance Attributes/app.py
+++ My Training/CodewithMosh/7. Classes/04-Class vs Instance Attributes/app.py
@@ -1,19 +1,20 @@
 class Point:
-    default_color = "red"
+    default_color = "red"  # class variable shared across all instances of a class
 
     def __init__(self, x, y):
-        self.x = x # instance attribute = these are attributes that belong to point instances
-        self.y = y # instance attribute = these are attributes that belong to point instances
+        self.x = x  # instance attribute = these are attributes that belong to point instances
+        self.y = y  # instance attribute = these are attributes that belong to point instances
 
-    def draw(self): # self is a reference to a current point object
-        print(f"Point ({self.x}, {self.y})") 
+    def draw(self):  # self is a reference to a current point object
+        print(f"Point ({self.x}, {self.y})")
+
 
 Point.default_color = "yellow"
-point = Point(1,2)
+point = Point(1, 2)
 print(point.default_color)
 print(Point.default_color)
 point.draw()
 
-another = Point(3,4)
+another = Point(3, 4)
 print(another.default_color)
 another.draw()
