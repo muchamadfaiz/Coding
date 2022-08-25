b685d82625ac07dd9b95f483d32c0cca831646c6
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-25 16:32:56 +0700

    Finish 07

--- My Training/CodewithMosh/7. Classes/07-Comparing Objects/app.py
+++ My Training/CodewithMosh/7. Classes/07-Comparing Objects/app.py
@@ -2,13 +2,20 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+        # return True
 
     def __gt__(self, other):
         return self.x > other.x and self.y > other.y
 
-point = Point(1, 2)
-other = Point(1, 2)
-print(point > other)
+    def __lt__(self, other):
+        return self.x < other.x and self.y < other.y
+
+
+point = Point(5, 2)
+other = Point(5, 2)
+print("gt hasilnya: {}".format(point > other))  # membandingkan objek
+print("eq hasilnya: {}".format(point == other))  # membandingkan objek
+print("lt hasilnya: {}".format(point < other))  # membandingkan objek
