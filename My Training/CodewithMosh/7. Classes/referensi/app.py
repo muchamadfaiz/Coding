c181dcd8eabee5fc0f96a754b7b1f1e357922fbd
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-25 23:53:52 +0700

    Referensi

---
+++ My Training/CodewithMosh/7. Classes/referensi/app.py
@@ -0,0 +1,20 @@
+# dict = {}
+# dict["python"] = 10
+# dict["a"] = 10
+# dict["b"] = 10
+# print(dict)
+
+# dict = {}
+# dict["python"] = dict.get("python", 0) + 1
+# dict["python"] = dict.get("python") + 1
+# dict["python"] = dict.get("python") + 1
+# print(dict)
+
+# print("python".lower())
+
+
+# class TagCloud:
+#     def __init__(self):
+#         self.tags = {}
+#  def add(self, tag):
+#         self.tags[tag] = self.tags.get(tag, 0) + 1
