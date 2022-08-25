b74f7b8342929c793e1ce9963fc95ef716fdd5ac
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-25 08:03:20 +0700

    Finish 07

---
+++ My Training/Kelas-Terbuka-OOP/07-Private Variable/app.py
@@ -0,0 +1,26 @@
+# Private Variable
+
+class Hero:
+    # class variable
+    jumlah = 0
+    __privateJumlah = 0
+
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+        # variabel instance private
+        self.__private = "private"
+        # variabel instance protected
+        self._protected = "protected"
+
+
+lina = Hero("lina", 100)
+
+# print(lina.__dict__)
+# print(lina.health)
+# print(lina.name)
+# print(lina.__private)
+
+print(Hero.__dict__)
+print(Hero.__privateJumlah)
