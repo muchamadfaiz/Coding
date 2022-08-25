5e853e68ffb6f27d6afe89042e73b9e08e750861
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-25 16:15:35 +0700

    Finish 06

--- My Training/CodewithMosh/7. Classes/06-Magic Methods/app.py
+++ My Training/CodewithMosh/7. Classes/06-Magic Methods/app.py
@@ -1,13 +1,14 @@
 class Point:
     def __init__(self, x, y):
-        self.x = x 
+        self.x = x
         self.y = y
 
-    def __str__(self):
+    def __str__(self):  # conver an object to a string
         return f"({self.x}, {self.y})"
 
-    def draw(self): 
-        print(f"Point ({self.x}, {self.y})") 
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
 
-point = Point(1,2)
+
+point = Point(1, 2)
 print(str(point))
