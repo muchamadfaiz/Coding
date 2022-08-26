aa64aad522872ccf298d6e97a7c3cc32806e012b
muchamadfaiz <muchamadfaiz@gmail.com>
2022-08-26 11:53:50 +0700

    Finish 03

---
+++ My Training/Ridwanilyas/03-Membuat Method/app.py
@@ -0,0 +1,10 @@
+class Mahasiswa:
+    pass
+
+    def sapa(self):
+        print("halo")
+
+
+faiz = Mahasiswa()
+# faiz.sapa() = "print halo" # ini tidak bisa karena untuk asign method class harus di dalam clasnya
+faiz.sapa()
