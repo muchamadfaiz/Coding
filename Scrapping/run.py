b68cad679a71750f0ec50838cd73977f08735685
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-05 13:39:46 +0700

    Finish Minggu ke 2

--- Scrapping/run.py
+++ Scrapping/run.py
@@ -1,3 +1,18 @@
+# import requests
+# from bs4 import BeautifulSoup
+# import csv
+
+# key = "hotels"
+# location = "london"
+# url = "https://www.yell.com/ucs/UcsSearchAction.do?keywords={}&location={}&scrambleSeed=86061706&pageNum=".format(key, location)
+# # headers = {
+# # ""
+# # }
+# req = requests.get(url)
+# print(req)
+
+
 import requests
-from bs4 import BeautifulSoup
-import csv
+
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+r.status_code
