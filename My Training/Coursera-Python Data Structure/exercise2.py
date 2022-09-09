a39c7b2bdd81582757efb261066cb026f830c529
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-09 16:39:30 +0700

    Minggu ke-2

---
+++ My Training/Coursera-Python Data Structure/exercise2.py
@@ -0,0 +1,20 @@
+fname = "mbox-short.txt"
+file = open(fname)
+count = 0
+listjumlah = []
+for line in file:
+    line = line.strip()
+    # print("line", line)
+    line1 = line.split()
+    # print("line1", line1)
+    # print(line1)
+    if len(line1) < 1:
+        continue
+    if line1[0] == "From" : 
+        count += 1
+        listjumlah.append(line1)
+        print(line1[1])
+    else:
+        continue
+
+print("There were {} lines in the file with From as the first word".format(len(listjumlah)))
