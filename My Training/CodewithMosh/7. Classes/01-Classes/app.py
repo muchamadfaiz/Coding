0463e7e3d32a4c55c1ec3a332835780bc6786504
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-11 14:01:22 +0700

    Finish 01

--- My Training/CodewithMosh/7. Classes/01-Classes/app.py
+++ My Training/CodewithMosh/7. Classes/01-Classes/app.py
@@ -1,8 +1,16 @@
-# Class : is a bule print for creating new object
+# Class : is a blueprint for creating new object
 # object : instance of a class
 
 # Class : Human
 # Objects : John, Mary, Jack
 
-x = 1
-print(type(x))
+# x = 1
+# print(type(x))
+
+numbers = [1,2]
+numbers.append("pawpw")
+numbers[1] = "bilbil"
+print(numbers)
+
+# Every object in python is created using a class which is 
+# blueprint for creating object of that type
