b3616e68c722fb20ae023bf3faed0fde493d1586
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-14 17:10:28 +0700

    check

---
+++ Scrapping/htmlparser.py
@@ -0,0 +1,8 @@
+# Parse website data using HTML Parser (ex. bs4 lib)
+from htmlparser import BeautifulSoup
+from urllib.request import urlopen
+
+url = "http://olympus.realpython.org/profiles/dionysus"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
