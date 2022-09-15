62dc18abce3f352143fbab5d05dd1696e7d3a982
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-15 15:32:48 +0700

    Finsih 06

---
+++ My Training/CodewithMosh/6. Exceptions/06-Raising Exception/app.py
@@ -0,0 +1,12 @@
+from distutils.log import error
+
+
+def calculate_xfactor(age):
+    if age <= 0 :
+        raise ValueError("Age cannot be 0 or less")
+    return 10 / age
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
