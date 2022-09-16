f9b75031c0226ace810c7b1b4bbb3ef5ecd630b3
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-16 07:13:14 +0700

    finish

--- Scrapping/main.py
+++ Scrapping/main.py
@@ -1,3 +1,4 @@
+# Parsing website with handling error
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.error import HTTPError
@@ -5,12 +6,11 @@ from urllib.error import URLError
 
 # membuka url
 try:
-    url = "http://olympus.realpython.org/profiles/dionysus"
+    url = "http://olympuss.realpython.org/profiles/dionysus"
     urlhandle = urlopen(url)
-except HTTPError:
-    
+except HTTPError as e:
+    print(e)
+else:
 # proses scrapping
-# soup = BeautifulSoup(urlhandle,"html.parser")
-# print(soup.head)
-
-
+    soup = BeautifulSoup(urlhandle,"html.parser")
+    print(soup.head)
