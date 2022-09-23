015c97fe00c0c3494346d995f39fdc4c152c4e2c
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-23 10:17:22 +0700

    progress

---
+++ My Training/CodewithMosh/9. Python Standard Library/02-Working with Directories/app.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+Path.cwd()
+# path = Path("ecommerce")
+# # path.exists() # return boolean
+# # path.mkdir() #making directory
+# # path.rmdir() #remove directory
+# # path.rename("ecommerce2") #rename directory
+
+# for p in (path.iterdir()): #get a list of file and directories in this path
+#     print(p)
