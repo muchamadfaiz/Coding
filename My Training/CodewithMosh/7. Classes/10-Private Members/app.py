155bc58784f1363a997e10036b02b7c2017ec84f
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-23 16:22:28 +0700

    Finish 10

--- My Training/CodewithMosh/7. Classes/10-Private Members/app.py
+++ My Training/CodewithMosh/7. Classes/10-Private Members/app.py
@@ -1,25 +1,22 @@
 class TagCloud:
     def __init__(self):
-        self.tags = {}
+        self.__tags = {}
 
     def add(self, tag):
-        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
 
     def __getitem__(self, tag):
-        return self.tags.get(tag.lower(), 0)
+        return self.__tags.get(tag.lower(), 0)
 
     def __setitem__(self, tag, count):
-        self.tags[tag.lower()] = count
+        self.__tags[tag.lower()] = count
 
     def __len__(self):
-        return len(self.tags)
+        return len(self.__tags)
 
     def __iter__(self):
-        return iter(self.tags)
+        return iter(self.__tags)
 
 
 cloud = TagCloud()
-cloud.add("python")
-cloud.add("python")
-cloud.add("Python")
-print(cloud['PYTHON'])
+print(cloud.__dict__)
