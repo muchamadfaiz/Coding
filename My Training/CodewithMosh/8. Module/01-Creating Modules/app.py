f426f8875c83dd30e51ee28793117201166ba323
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-24 19:57:06 +0700

    Finish 01

--- My Training/CodewithMosh/8. Module/01-Creating Modules/app.py
+++ My Training/CodewithMosh/8. Module/01-Creating Modules/app.py
@@ -1,1 +1,27 @@
-# Creating modules
+"""here we want to import our module called sales.py
+
+from "modul_name" import "object_name/function_name, otherobject_name"
+in python all is an object, variable is object, function is object etc
+we can import single or multiple object using a comma
+dont import all module using (*) just import that you need because 
+it can overwrite the object with the same name on that module"""
+from sales import calc_shipping, calc_tax
+
+# other way to import the module
+# instead of we starting with "from" we start with "import"
+# import "modul_name"
+import sales # so now in this module we have an object called sales
+# and the we can access its member with (.)
+sales.calc_tax()
+sales.calc_shipping()
+
+calc_shipping()
+calc_tax()
+
+"""summary
+- so basically we can either import the entire module as an object -> 
+import sales
+sales.calc_tax()
+- or we can import specific object from that module
+from sales import calc_tax
+calc_tax()"""
---
+++ My Training/CodewithMosh/8. Module/01-Creating Modules/sales.py
@@ -0,0 +1,14 @@
+# Creating modules
+# this is a module with name sales.py
+# module is a file that contains some python code
+# what function to put in the module?
+# the answer is like in the shopping mart there an isles for every product
+# in fruit section we find fruit product, you dont see cleaning product there
+# and same as here we put objects(function, class, variable and so on) that has related each other
+
+# in naming module the convention is lowercase and separated by underscore
+def calc_tax():
+    pass
+
+def calc_shipping():
+    pass
