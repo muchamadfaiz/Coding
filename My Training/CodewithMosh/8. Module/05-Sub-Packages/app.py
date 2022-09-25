39d744e68d8fca9782df80bef6381c8178792815
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-25 12:18:53 +0700

    Finsih 05

---
+++ My Training/CodewithMosh/8. Module/05-Sub-Packages/app.py
@@ -0,0 +1,6 @@
+# Sub-Packages
+# as yout program grow you may want to break a package down into sub-packages
+# so lets add a new file called shopping a sub directory of ecommerce package
+# after that to make ashopping directory as a package we make a file python called __init__.py
+
+from ecommerce.shopping import sales
