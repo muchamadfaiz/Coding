118a6c9b3ada3852683251f4d5c22d118976145e
muchamadfaiz <muchamadfaiz@gmail.com>
2022-09-25 11:49:12 +0700

    Finish 03

---
+++ My Training/CodewithMosh/8. Module/03-Module Search Path/app.py
@@ -0,0 +1,20 @@
+# MODULE SEARCH PATH
+# its a path where python will find the module that you calling
+# if it did'nt find in the first directory they will search next to it and so on
+
+import sales
+import sys
+
+print(sys.path)
+
+"""
+['d:\\Coding\\My Training\\CodewithMosh\\8. Module\\03-Module Search Path', # This is first director that pyton will search the called module
+ 'C:\\Python310\\python310.zip',
+  'C:\\Python310\\DLLs',
+   'C:\\Python310\\lib',
+    'C:\\Python310',
+    'C:\\Python310\\lib\\site-packages',
+     'C:\\Python310\\lib\\site-packages\\win32',
+      'C:\\Python310\\lib\\site-packages\\win32\\lib',
+       'C:\\Python310\\lib\\site-packages\\Pythonwin']
+"""
---
+++ My Training/CodewithMosh/8. Module/03-Module Search Path/sales.py
@@ -0,0 +1,6 @@
+def calc_shipping():
+    print("pawpaw")
+
+
+def calc_tax():
+    pass
